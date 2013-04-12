905b6a51b2a39082390cf1c3142422865cf27b2d
Willem Allan <willemarf@gmail.com>
2013-04-12 09:29:23 -0300

    alterando fabfile

--- fabfile.py
+++ fabfile.py
@@ -39,6 +39,7 @@ def newserver():
     run('mv /etc/supervisor/supervisord_server.conf /etc/supervisor/supervisord.conf')
     supervisor_restart()
 
+# cria projeto local
 def newproject():
     """ Criando novo projeto """
     log('Criando novo projeto')
@@ -56,15 +57,39 @@ def newproject():
     local('git init')
     local('git remote add origin ssh://git@bitbucket.org/willemarf/{0}.git'.format(conta))
 
+# cria uma conta no servidor
 def novaconta():
     """Criando uma nova conta do usuário"""
     log('Criando uma nova conta do usuário')
 
     # criando usuario
     conta = raw_input('Digite o nome da conta: ')
-    adduser(conta)
 
-def gera_senha(tamanho):
+    # cria usuario no linux
+    user_senha = gera_senha(12)
+    adduser(conta, user_senha)
+
+    run('mkdir /home/{0}/logs'.format(conta))
+    run('touch /home/{0}/logs/access.log'.format(conta))
+    run('touch /home/{0}/logs/error.log'.format(conta))
+    run('virtualenv /home/{0}/env --no-site-packages'.format(conta))
+    local('scp nginx.conf {0}:/home/{1}'.format(prod_server, conta))
+    local('scp supervisor.conf {0}:/home/{1}'.format(prod_server, conta))
+
+    # cria banco e usuario no banco
+    banco_senha = gera_senha(12)
+    newbase(conta, banco_senha)
+
+    # da permissao para o usuario no diretorio
+    run('sudo chown -R {0}:{0} /home/{0}'.format(conta))
+
+    # log para salvar no docs
+    log('Anotar dados da conta: {0}'.format(conta))
+    print 'USUÁRIO senha: {0}'.format(user_senha)
+    print 'BANCO senha: {0}'.format(banco_senha)
+
+# gera senha
+def gera_senha(tamanho=12):
     """Gera uma senha - parametro tamanho"""
     from random import choice
     caracters = '0123456789abcdefghijlmnopqrstuwvxzkABCDEFGHIJLMNOPQRSTUWVXZK_#'
@@ -73,105 +98,101 @@ def gera_senha(tamanho):
         senha += choice(caracters)
     return senha
 
-
-def adduser(conta):
+# cria usuario no servidor
+def adduser(conta=None, user_senha=None):
     """Criando usuário"""
-    user_senha = gera_senha(12)
-    banco_senha = gera_senha(12)
+
+    if not user_senha:
+        user_senha = gera_senha(12)
     print 'senha usuário: {0}'.format(user_senha)
 
+    if not conta:
+        conta = raw_input('Digite o nome do banco: ')
+
     log('Criando usuário {0}'.format(conta))
-    with cd(project_path):
-        run('sudo adduser {0}'.format(conta))
-        run('mkdir /home/{0}/logs'.format(conta))
-        run('touch /home/{0}/logs/access.log'.format(conta))
-        run('touch /home/{0}/logs/error.log'.format(conta))
-        run('virtualenv /home/{0}/env --no-site-packages'.format(conta))
-        local('scp nginx.conf {0}:/home/{1}'.format(prod_server, conta))
-        local('scp supervisor.conf {0}:/home/{1}'.format(prod_server, conta))
+    run('sudo adduser {0}'.format(conta))
 
-        run("echo CREATE DATABASE {0} | mysql -u root -p".format(conta))
-        run("echo \"CREATE USER '{0}'@'localhost' IDENTIFIED BY '{1}'\" | mysql -u root -p".format(conta, banco_senha))
-        run("echo \"GRANT ALL PRIVILEGES ON {0} . * TO '{0}'@'localhost'\" | mysql -u root -p".format(conta))
 
-        run('sudo chown -R {0}:{0} /home/{0}'.format(conta))
+# deleta uma conta no servidor
+def delconta():
+    """Deletando conta"""
+    conta = raw_input('Digite o nome da conta: ')
+    log('Deletando conta {0}'.format(conta))
+    userdel(conta)
+    dropbase(conta)
 
-        log('Anotar dados da conta: {0}'.format(conta))
-        print 'USUÁRIO senha: {0}'.format(user_senha)
-        print 'BANCO senha: {0}'.format(banco_senha)
 
-def newbase():
+# MYSQL - cria usuario e banco de dados
+def newbase(conta=None, banco_senha=None):
     """NEW DATABASE"""
-    banco_senha = gera_senha(12)
-    conta = raw_input('Digite o nome do banco: ')
+
+    if not banco_senha:
+        banco_senha = gera_senha(12)
+    print 'Senha gerada para o banco: {0}'.format(banco_senha)
+
+    if not conta:
+        conta = raw_input('Digite o nome do banco: ')
     log('NEW DATABASE {0}'.format(conta))
 
     run("echo CREATE DATABASE {0} | mysql -u root -p".format(conta))
     run("echo \"CREATE USER '{0}'@'localhost' IDENTIFIED BY '{1}'\" | mysql -u root -p".format(conta, banco_senha))
     run("echo \"GRANT ALL PRIVILEGES ON {0} . * TO '{0}'@'localhost'\" | mysql -u root -p".format(conta))
 
-def dropbase():
+
+# MYSQL - deleta o usuario e o banco de dados
+def dropbase(conta=None):
     """DROP DATABASE"""
-    conta = raw_input('Digite o nome do banco: ')
+    if not conta:
+        conta = raw_input('Digite o nome do banco: ')
     run("echo DROP DATABASE {0} | mysql -u root -p".format(conta))
     run("echo \"DROP USER '{0}'@'localhost'\" | mysql -u root -p".format(conta))
 
-def userdel(conta):
+# LINUX - deleta o usuario
+def userdel(conta=None):
     """Deletando usuário"""
+    if not conta:
+        conta = raw_input('Digite o nome do usuario: ')
     log('Deletando usuário {0}'.format(conta))
-    with cd(project_path):
-        run('sudo userdel -r {0}'.format(conta))
+    run('sudo userdel -r {0}'.format(conta))
 
-def delconta():
-    """Deletando conta"""
-    conta = raw_input('Digite o nome da conta: ')
-    log('Deletando conta {0}'.format(conta))
-    userdel(conta)
-    run("echo DROP DATABASE {0} | mysql -u root -p".format(conta))
-    run("echo \"DROP USER '{0}'@'localhost'\" | mysql -u root -p".format(conta))
-
+# update no servidor
 def update():
     """Atualizando pacotes"""
     log('Atualizando pacotes')
-    with cd(project_path):
-        run('sudo apt-get update')
+    run('sudo apt-get update')
 
-
+# upgrade no servidor
 def upgrade():
     """Atualizando programas"""
     log('Atualizando programas')
-    with cd(project_path):
-        run('sudo apt-get upgrade')
+    run('sudo apt-get upgrade')
 
 
 def build():
     """Instalando build-essential"""
     log('instalando build-essential gcc++')
-    with cd(project_path):
-        run('sudo apt-get install build-essential automake')
-        run('sudo apt-get install libxml2-dev libxslt-dev')
-        run('sudo apt-get install libjpeg-dev libjpeg8-dev zlib1g-dev libfreetype6 libfreetype6-dev')
+    run('sudo apt-get install build-essential automake')
+    run('sudo apt-get install libxml2-dev libxslt-dev')
+    run('sudo apt-get install libjpeg-dev libjpeg8-dev zlib1g-dev libfreetype6 libfreetype6-dev')
+
 
 def python():
     """Instalando todos pacotes necessários"""
     log('Instalando todos pacotes necessários')
-    with cd(project_path):
-        run('sudo apt-get install python python-dev python-setuptools python-mysqldb python-pip python-virtualenv')
-        run('pip install -U distribute')
+    run('sudo apt-get install python python-dev python-setuptools python-mysqldb python-pip python-virtualenv')
+    run('pip install -U distribute')
 
 
 def mysql():
     """Instalando MySQL"""
     log('Instalando MySQL')
-    with cd(project_path):
-        run('sudo apt-get install mysql-server libmysqlclient-dev')
+    run('sudo apt-get install mysql-server libmysqlclient-dev')
 
 
 def outros():
     """Instalando git, nginx e supervisor"""
     log('Instalando git, nginx e supervisor')
-    with cd(project_path):
-        run('sudo apt-get install git nginx supervisor')
+    run('sudo apt-get install git nginx supervisor')
 
 
 def login():
@@ -187,6 +208,8 @@ def upload_public_key():
     put(ssh_file, target_path)
     run('echo `cat ~/.ssh/uploaded_key.pub` >> ~/.ssh/authorized_keys && rm -f ~/.ssh/uploaded_key.pub')
 
+
+# RESTART
 def restart():
     """reiniciando servicos"""
     log('reiniciando servicos')
@@ -197,80 +220,72 @@ def restart():
     supervisor_stop()
     supervisor_start()
     supervisor_restart()
-    # supervisor_reload()
 
 
-# SUPERVISOR
+# SUPERVISOR APP
 def start_server():
     """inicia aplicação"""
     conta = raw_input('Digite o nome da app: ')
     log('inicia aplicação')
-    with cd(project_path):
-        run('sudo supervisorctl start %s' % conta)
+    run('sudo supervisorctl start %s' % conta)
+
 
 def stop_server():
     """para aplicação"""
     conta = raw_input('Digite o nome da app: ')
     log('para aplicação')
-    with cd(project_path):
-        run('sudo supervisorctl stop %s' % conta)
+    run('sudo supervisorctl stop %s' % conta)
+
 
 def restart_server():
     """reinicia aplicação"""
     conta = raw_input('Digite o nome da app: ')
     log('reinicia aplicação')
-    with cd(project_path):
-        run('sudo supervisorctl restart %s' % conta)
+    run('sudo supervisorctl restart %s' % conta)
 
+
+# SUPERVISOR
 def supervisor_start():
     """start supervisor"""
     log('start supervisor')
-    with cd(project_path):
-        run('sudo /etc/init.d/supervisor start')
+    run('sudo /etc/init.d/supervisor start')
+
 
 def supervisor_stop():
     """stop supervisor"""
     log('stop supervisor')
-    with cd(project_path):
-        run('sudo /etc/init.d/supervisor stop')
+    run('sudo /etc/init.d/supervisor stop')
+
 
 def supervisor_restart():
     """restart supervisor"""
     log('restart supervisor')
-    with cd(project_path):
-        run('sudo /etc/init.d/supervisor restart')
-
-# def supervisor_reload():
-#     """reload supervisor"""
-#     log('reload supervisor')
-#     with cd(project_path):
-#         run('sudo /etc/init.d/supervisor reload')
+    run('sudo /etc/init.d/supervisor restart')
 
 
-# NGINX 
+# NGINX
 def nginx_start():
     """start NGINX"""
     log('start NGINX')
-    with cd(project_path):
-        run('sudo /etc/init.d/nginx start')
+    run('sudo /etc/init.d/nginx start')
+
 
 def nginx_stop():
     """stop NGINX"""
     log('stop NGINX')
-    with cd(project_path):
-        run('sudo /etc/init.d/nginx stop')
+    run('sudo /etc/init.d/nginx stop')
+
 
 def nginx_restart():
     """restart NGINX"""
     log('restart NGINX')
-    with cd(project_path):
-        run('sudo /etc/init.d/nginx restart')
+    run('sudo /etc/init.d/nginx restart')
+
 
 def nginx_reload():
     """reload NGINX"""
     log('reload NGINX')
-    with cd(project_path):
-        run('sudo /etc/init.d/nginx reload')
+    run('sudo /etc/init.d/nginx reload')
 
 
 def log(message):
