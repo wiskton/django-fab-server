a7f5a2b648e4690e7f48cdcecc5438313c9da2ab
Willem Allan <willemarf@gmail.com>
2013-10-05 01:34:43 -0300

    ip do servidor

--- fabfile.py
+++ fabfile.py
@@ -12,7 +12,7 @@ CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
 # --------------------------------------------------------
 
 username = 'root'
-ip = '198.199.77.160'
+ip = '192.168.0.1'
 prod_server = '{0}@{1}'.format(username, ip)
 project_path = '/home/'
 # env_path = '/home/'
